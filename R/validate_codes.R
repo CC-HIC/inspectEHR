de953c3c6a36a2226f15ef0c85ac9a12e1c04be6
Edward Palmer <edward.palmer@ucl.ac.uk>
2019-08-06 14:01:39 +0100

    Added synthetic generators for NHS numbers and ICNARC codes

--- R/validate_codes.R
+++ R/validate_codes.R
@@ -60,6 +60,46 @@ validate_nhs <- function(nhs_numbers = NULL) {
 }
 
 
+generate_nhs <- function(size = 1) {
+
+  nhs_numbers <- vector(mode = "character", length = size)
+
+  for (j in seq_len(size)) {
+
+    valid <- FALSE
+
+    while (!valid) {
+
+      start_seq <- sample(x = 1:9, size = 9, replace = TRUE)
+      response <- vector(mode = "integer", length = 9)
+      multiplier <- 10
+
+      for (i in seq_along(response)) {
+        response[i] <- start_seq[i] * multiplier
+        multiplier <- multiplier - 1
+      }
+
+      totals <- sum(response)
+      residual <- totals %% 11
+      last_digit <- 11 - residual
+
+      if (last_digit == 11) {
+        last_digit <- 0
+      } else if (last_digit == 10) {
+        next
+      }
+
+    valid <- TRUE
+    }
+
+    nhs_numbers[j] <- paste0(c(start_seq, last_digit), collapse = "")
+
+    }
+
+    return(nhs_numbers)
+}
+
+
 
 validate_post_code <- function(post_code = NULL) {
 
@@ -78,10 +118,48 @@ validate_post_code <- function(post_code = NULL) {
 
 validate_icnarc <- function(icnarc_code = NULL) {
 
-  test_pattern <- "([1-2].[1-12].[1-13].[1-54].[1-17])"
-  result <- grepl(pattern = test_pattern, x = icnarc_code)
+  icnarc <- stringr::str_split(icnarc_code, fixed(".")) %>%
+    purrr::map(as.integer) %>%
+    purrr::map(function(x) {
+      if (length(x) != 5) {
+        return(FALSE)
+      } else {
+        pos_a <- x[1] %in% c(1:2)
+        pos_b <- x[2] %in% c(1:12)
+        pos_c <- x[3] %in% c(1:13)
+        pos_d <- x[4] %in% c(1:54)
+        pos_e <- x[5] %in% c(1:17)
+        result <- all(c(pos_a, pos_b, pos_c, pos_d, pos_e))
+        return(result)
+      }
+    }) %>%
+    unlist()
 
-  return(result)
+  return(icnarc)
+
+}
+
+
+generate_icnarc <- function(size = 1) {
+
+  icnarc <- vector(mode = "character", length = size)
+
+  for (i in seq_len(size)) {
+
+    x <- vector(mode = "integer", length = 5)
+
+    x[1] <- sample(1:2, size = 1)
+    x[2] <- sample(1:12, size = 1)
+    x[3] <- sample(1:13, size = 1)
+    x[4] <- sample(1:54, size = 1)
+    x[5] <- sample(1:17, size = 1)
+
+    x <- paste0(str_pad(x, width = 2, side = "left", pad = "0"), collapse = ".")
+    icnarc[i] <- x
+
+  }
+
+  return(icnarc)
 
 }
 
