77c96cbeb59499340644045661fb9bbba70bc3c5
Edward Palmer <edward.palmer@ucl.ac.uk>
2019-08-27 16:19:33 +0100

    removed some old functions

--- data-raw/working.R
+++ data-raw/working.R
@@ -34,28 +34,3 @@ ks_test(dt_1d)
 
 report(sqlite_file = "./data-raw/synthetic_db.sqlite3",
        output_folder = "~/Documents/academic/cc-hic/dq-demo")
-str_1d <- extract(core, input = "NIHR_HIC_ICU_0074") %>% verify_events(verified_episodes)
-
-df <- extract(core_table = core, input = "NIHR_HIC_ICU_0108")
-
-# Check to see if there is any data there
-if (nrow(df) == 0 || sum(is.na(df$value)) == nrow(df)) {
-  rlang::inform(glue("Skipping over {hic_codes[i]} as no data present"))
-} else {
-
-  # Basic Verification: range, boundary, duplcation, periodicity
-  df <- verify_events(df, verified_episodes)
-
-  # Coverage verification
-  df_cov <- coverage(df, reference_tbl = reference)
-
-  # Statistical verification
-  ks_pos <- qref[qref$code_name == hic_codes[i], "dist_compare", drop = TRUE]
-  if (!is.na(ks_pos) && ks_pos == "ks") {
-    df_stat <- ks_test(df)
-  } else {
-    df_stat <- NULL
-  }
-
-  # Saving errors outside the main list
-  hic_event_summary[[hic_codes[i]]] <- summarise_verification(df, df_stat, df_cov, reference)
