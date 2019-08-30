c85f7a312766cc8fa0a673ba35450979b0134662
Edward Palmer <edward.palmer@ucl.ac.uk>
2019-08-30 14:14:59 +0100

    Fixed tests

--- tests/testthat/test_characterise_episodes.R
+++ tests/testthat/test_characterise_episodes.R
@@ -7,8 +7,8 @@ episodes <- characterise_episodes(ctn)
 DBI::dbDisconnect(ctn)
 
 test_that("Table properties are correct", {
-  expect_true(data.frame %in% class(episodes))
-  expect_equal(dim(dtb), c(1000, 7))
+  expect_true("data.frame" %in% class(episodes))
+  expect_equal(dim(episodes), c(1000, 7))
   expect_equal(names(episodes),
                c("episode_id", "nhs_number", "epi_start_dttm",
                  "epi_end_dttm", "outcome", "los_days", "site"))
